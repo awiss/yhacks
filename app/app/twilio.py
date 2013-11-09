debc08585195e8c61261b8582ff5175387659477
Alex Wissmann <awiss@wharton.upenn.edu>
2013-11-08 22:21:33 -0500

    bleh

--- app/app/twilio.py
+++ app/app/twilio.py
@@ -1,1 +1,9 @@
-SHUT UP
+from django.http import HttpResponse,HttpResponseRedirect
+import requests
+import logging
+
+def firstText(request):
+	if request.method == 'POST':
+		pass
+	else:
+		return HttpResponse("Go away")
--- app/app/urls.py
+++ app/app/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+from app import twilio
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,5 +11,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url('', twilio.___),
+    url('', twilio.firstText),
 )
