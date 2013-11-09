934369e7f8dfac67c0e50420ae49f30f6a0c5bd6
Alex Wissmann <awiss@wharton.upenn.edu>
2013-11-09 00:21:59 -0500

    Removed some files

--- app/app/twilio.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse,HttpResponseRedirect
-import requests
-import logging
-
-def firstText(request):
-	if request.method == 'POST':
-		pass
-	else:
-		return HttpResponse("Go away")
