eb0c56c69c57c428ba972b98d1a54ec958ec1756
Alex Wissmann <awiss@wharton.upenn.edu>
2013-11-09 02:36:00 -0500

    log

--- app/routes/twilio.js
+++ app/routes/twilio.js
@@ -1,6 +1,7 @@
 
 var twilioClient = require('twilio')('ACcf46cc45dfc6c558215e76d503ee76de','ebdfcae31d9493e4859d3b80c2b2672b');
 exports.text = function(request,response){
+	console.log(request);
 	twilioClient.sendMessage({
 		to: request.body.from,
 		from: '+17209614567', 
